632726f42e9bfe781e870504f090061a2464e628
Himanshu Sharma <sharmahimanshu82194@gmail.com>
2026-04-28 01:29:20 +0530

    Refactor WSGI configuration; remove unnecessary comments and clean up code

--- RosterApp/roster_system/wsgi.py
+++ RosterApp/roster_system/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for roster_system project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -15,11 +6,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'roster_system.settings')
 
 application = get_wsgi_application()
 
-<<<<<<< HEAD:RosterApp/roster_system/wsgi.py
-=======
-<<<<<<< HEAD:roster_system/wsgi.py
-
+# Alias for deployment platforms that expect 'app'
 app = application
-=======
->>>>>>> 9779b07 (Add CSS styles and sample roster data):RosterApp/roster_system/wsgi.py
->>>>>>> 1e38777 (Add CSS styles and sample roster data):roster_system/wsgi.py
